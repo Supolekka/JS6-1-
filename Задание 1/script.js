4a1ab218f24116e389cbadb1230cdb3fa8ee9a43
Данил <82579479+Supolekka@users.noreply.github.com>
2023-01-16 10:27:19 +0500

    Add files via upload

--- Задание 1/script.js
+++ Задание 1/script.js
@@ -2,7 +2,7 @@
 var elem = document.getElementsByTagName('input');
 var a = document.getElementById('test');
 for (var i = 0; i < elem.length; i++) {
-	elem[i].addEventListener('focus', func);
+	elem[i].addEventListener('blur', func);
 }
 function func() {
 	a.innerHTML = this.value;
